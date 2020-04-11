2965a185fe1d5d94ae0828fb28b8bc9005802c2a
Emma <enabojohn@gmail.com>
2020-04-12 00:24:11 +0300

    Fetching from the database into ideas/index file

--- app.js
+++ app.js
@@ -64,7 +64,15 @@ app.post('/ideas', (req, res) => {
         });
     }
      else {
-        res.send('Passed');
+         const newUser = {
+             title: req.body.title,
+             details: req.body.details
+         };
+        new Idea(newUser)
+        .save()
+        .then(idea => {
+            res.redirect('/ideas');
+        });
     }
 });
 
