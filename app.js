db2b56e629403284181660008c8d39b39ef63022
Emma <enabojohn@gmail.com>
2020-04-11 09:42:51 +0300

    Created mongoose connection to the mongoDb

--- app.js
+++ app.js
@@ -6,8 +6,13 @@ const mongoose = require('mongoose');
 
 //Innitialise app variable
 const app = express();
+
+//Map global promise-get rid of promise
+mongoose.Promise = global.Promise;
 //Connect to mongoose
-mongoose.connect('mongodb://localhost/vidjot-dev');
+mongoose.connect('mongodb://localhost/vidjot-dev', { useNewUrlParser: true })
+    .then(() => console.log('MongoDB connected...'))
+    .catch(err => console.log(err));
 app.use(express.static(__dirname));
 app.use(bodyParser.urlencoded({ extended: true }));
 
