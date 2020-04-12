6849ec3095a84a668d7fc7d55a7bbbd91431e8be
Emma <enabojohn@gmail.com>
2020-04-12 22:26:21 +0300

    Added session and flash

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const path = require('path');
 const express =  require('express');
 const exphbs = require('express-handlebars');
 const methodOverride = require('method-override');
+const flash = require('connect-flash');
+const session  = require('express-session');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
@@ -26,6 +28,25 @@ app.use(bodyParser.json());
 
 //Method override middleware
 app.use(methodOverride('_method'));
+
+//Express-session middleware
+app.use(session({
+    secret:'keyboard cat',
+    resave: true,
+    saveUninitialized: true
+}));
+
+// app.use(require('express-session')({secrete: 'keyboard cat', resave:true, saceUninitialized:true}));
+//Configuring flash
+app.use(flash());
+
+//Global variables
+app.use (function(req, res, next){
+    res.locals.success_msg = req.flash('success_msg');
+    res.locals.error_msg = req.flash('error_msg');
+    res.locals.error = req.flash('error');
+    next();
+});
 
 //Handle bars middle ware(setting up engine to handle bars)
 app.engine('handlebars', exphbs({ 
@@ -101,6 +122,7 @@ app.post('/ideas', (req, res) => {
         new Idea(newUser)
         .save()
         .then( idea => { 
+            req.flash('success_msg', 'Video Idea has been Added');
             res.redirect('/ideas');
         });
     }
@@ -118,26 +140,29 @@ app.put('/ideas/:id', (req, res) => {
 
         idea.save()
         .then(idea => {
+            req.flash('success_msg', 'Video idea has been updated');
             res.redirect('/ideas');
         });
     });
 });
 
+
 //Delete Idea
 app.delete('/ideas/:id', (req, res) => {
    Idea.remove({ _id:req.params.id})
    .then(()=> {
+       req.flash('success_msg', 'Video Idea has been removed');
         res.redirect('/ideas');
    });
 });
+
 
 // Setting up a listening port to 5000 
 const port = 5000;
 app.listen(port, () => {
     console.log(`Server started on port ${port}...`);
 });
-
-//  THE END
+//THE END
 
 
 
